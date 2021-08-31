c2d7dd30d4bfcb47bea9fe76702e226964df1379
olixina <695176656@qq.com>
2021-08-31 19:37:40 +0800

    feat: update

--- docs/.vuepress/config/sidebarConfig.js
+++ docs/.vuepress/config/sidebarConfig.js
@@ -1,34 +1,34 @@
 module.exports = {
-    '/yuedu/': [
+    '': [
         {
             title: '指南',
             collapsable: false,
             children: [
-                '',
-                '教程',
+                '/yuedu/',
+                '/yuedu/教程',
             ]
         },
         {
             title: '说明',
             collapsable: false,
             children: [
-                '个人备份说明',
-                '备份文件说明',
+                '/yuedu/个人备份说明',
+                '/yuedu/备份文件说明',
             ]
         },
         {
             title: '资源',
             collapsable: false,
             children: [
-                '资源共享',
+                '/yuedu/资源共享',
             ]
         },
         {
             title: '工具',
             collapsable: false,
             children: [
-                '关耳美化发现工具',
-                '书源校验工具',
+                '/yuedu/关耳美化发现工具',
+                '/yuedu/书源校验工具',
             ]
         }
     ]
